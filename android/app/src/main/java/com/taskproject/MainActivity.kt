fd390b3726c1c44e4e8387e48c0e0e4efa7b1972
hashaam <hashaamkhan4247@gmail.com>
2024-03-17 16:32:33 +0500

    pr initialization, app icon, splash screen

--- android/app/src/main/java/com/taskproject/MainActivity.kt
+++ android/app/src/main/java/com/taskproject/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.taskproject
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.ReactRootView
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import com.facebook.react.shell.MainReactPackage
+import org.devio.rn.splashscreen.SplashScreen // Import SplashScreen from react-native-splash-screen package
 
-class MainActivity : ReactActivity() {
+class MainActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
+    override fun getMainComponentName(): String = "taskProject"
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "taskProject"
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : ReactActivityDelegate(this, mainComponentName) {
+            override fun onCreate(savedInstanceState: Bundle?) {
+                SplashScreen.show(this@MainActivity) // Show splash screen
+                super.onCreate(savedInstanceState)
+            }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+            override fun onPause() {
+                super.onPause()
+                val reactContext: ReactContext = reactInstanceManager.currentReactContext ?: return
+                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                        .emit("onPause", null)
+            }
+
+            override fun onResume() {
+                super.onResume()
+                val reactContext: ReactContext = reactInstanceManager.currentReactContext ?: return
+                reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                        .emit("onResume", null)
+            }
+        }
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
 }
