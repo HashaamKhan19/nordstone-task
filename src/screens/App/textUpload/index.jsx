1a5707c40d5e9208568edaa09a48f4c908b17f0e
hashaam <hashaamkhan4247@gmail.com>
2024-03-20 19:53:19 +0500

    feat: text-upload

--- src/screens/App/textUpload/index.jsx
+++ src/screens/App/textUpload/index.jsx
@@ -1,10 +1,13 @@
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import fonts from '../../../utils/fonts';
 import TextField from '../../../components/shared/TextField';
 import dimensions from '../../../utils/dimensions';
 import BasicButton from '../../../components/shared/Button';
 import {uploadText} from '../../../services/firebase/textUpload';
+import {notification} from '../../../components/notification';
+import deviceStorage from '../../../utils/deviceStorage';
+import firestore from '@react-native-firebase/firestore';
 
 const TextUpload = () => {
   const [text, setText] = useState('');
@@ -12,33 +15,78 @@ const TextUpload = () => {
   const [list, setList] = useState([]);
 
   const handleUpload = async () => {
+    setLoading(true);
+    if (!text) {
+      notification(
+        (type = 'warning'),
+        (title = 'Empty Field!'),
+        (textBody = 'Please write something to upload!'),
+        1000,
+      );
+      setLoading(false);
+      return;
+    }
+
     try {
-      setLoading(true);
       await uploadText(text);
     } catch (error) {
       console.log('error', error);
     } finally {
       setLoading(false);
-      // setText('');
+      setText('');
     }
   };
 
+  const getData = async () => {
+    const userInfoString = await deviceStorage.loadItem('user');
+    if (!userInfoString) {
+      setLoading(false);
+      return null;
+    }
+    firestore()
+      .collection('Text')
+      .where('email', '==', userInfoString?.email)
+      .onSnapshot(querySnapshot => {
+        const documents = [];
+        querySnapshot.forEach(doc => {
+          documents.push({...doc.data()});
+        });
+        setList(documents);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log('list of text', list);
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.txt}>Upload a text</Text>
+    <View style={styles.container}>
+      <View style={styles.secCont}>
+        <Text style={styles.txt}>Upload a text</Text>
 
-      <View style={styles.body}>
-        <TextField
-          value={text}
-          onChange={text => setText(text)}
-          label={'Write something'}
-        />
-      </View>
+        <View style={styles.body}>
+          <TextField
+            value={text}
+            onChange={text => setText(text)}
+            label={'Write something'}
+          />
+        </View>
 
-      <View style={styles.btn}>
         <BasicButton text={'Upload'} onPress={handleUpload} loading={loading} />
       </View>
-    </ScrollView>
+
+      <ScrollView contentContainerStyle={styles.mytext}>
+        <Text style={styles.txt}>My Texts</Text>
+
+        {list.map((item, index) => (
+          <Text key={index} style={styles.mapping}>
+            {item.text}
+          </Text>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
@@ -48,12 +96,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  secCont: {
+    height: dimensions.Height * 0.5,
+    paddingBottom: dimensions.Height * 0.04,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
   body: {
     marginTop: dimensions.Height * 0.04,
   },
   txt: {
     fontSize: fonts.size.font18,
     fontFamily: 'Urbanist-Bold',
+  },
+  mytext: {
+    width: dimensions.Width * 0.9,
+    overflow: 'scroll',
+    marginBottom: dimensions.Height * 0.04,
+    paddingBottom: dimensions.Height * 0.04,
+  },
+  mapping: {
+    fontSize: fonts.size.font17,
+    fontFamily: 'Urbanist-Medium',
+    marginVertical: dimensions.Height * 0.01,
   },
 });
 
--- src/services/firebase/textUpload.js
+++ src/services/firebase/textUpload.js
@@ -1,18 +1,15 @@
 import firestore from '@react-native-firebase/firestore';
 import deviceStorage from '../../utils/deviceStorage';
+import {notification} from '../../components/notification';
 
 export const uploadText = async text => {
-  console.log('text inside service', text);
   try {
-    console.log('inside try');
     const userInfoString = await deviceStorage.loadItem('user');
     if (!userInfoString) {
       return null;
     }
-    console.log('user info in try', userInfoString);
 
     const email = userInfoString?.email;
-    console.log('email in try', email);
     firestore()
       .collection('Text')
       .add({
@@ -22,9 +19,21 @@ export const uploadText = async text => {
       })
       .then(() => {
         console.log('Text Stored');
+        notification(
+          (type = 'success'),
+          (title = 'Text Uploaded'),
+          (textBody = 'Your text has been uploaded successfully!'),
+          1000,
+        );
       })
       .catch(error => {
         console.log('Error adding document: ', error);
+        notification(
+          (type = 'error'),
+          (title = 'Error'),
+          (textBody = 'Error while uploading text'),
+          1000,
+        );
       });
   } catch (err) {
     console.log(err);
